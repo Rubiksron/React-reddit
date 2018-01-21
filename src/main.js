c86d347f0a64c54f4529f84921ea6a3a7797d683
Rubiksron <rubiksron@gmail.com>
2018-01-20 22:08:54 -0800

    adding files for yellow world, getting up and running, now I have scss loading along with a title.

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import './style/main.scss'
+import React from 'react'
+import ReactDom from 'react-dom'
+
+class App extends React.Component {
+  constructor(props){
+    super(props)
+  }
+
+  render(){
+    return (
+      <main>
+        <h1> apple sauce </h1>
+      </main>
+    )
+  }
+}
+
+ReactDom.render(<App />, document.getElementById('root'))
